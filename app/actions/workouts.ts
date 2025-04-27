40dce770483dede73ee0b9f1d0253ab3e341ed0a
abdelrahim <abdelrhiimabdelmoneim@outlook.com>
2025-04-27 16:40:52 +0300

    Refactor import path for authentication in workouts action file

--- app/actions/workouts.ts
+++ app/actions/workouts.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { auth } from "@/app/api/auth/[...nextauth]/route";
+import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
