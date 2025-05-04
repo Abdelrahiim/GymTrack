5bda7dbd2abf87ce53a666f3c5418d20d5c38938
abdelrahim <abdelrhiimabdelmoneim@outlook.com>
2025-05-04 14:02:53 +0300

    Refactor import statement in NewUserForm component for consistency
    - Changed the import of zod from a namespace import to a default import, aligning with coding standards and improving clarity.

--- components/admin/NewUserForm.tsx
+++ components/admin/NewUserForm.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { createUser } from "@/actions/users"; // Server Action
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
+import z from "zod";
 import {
   Form,
   FormControl,
